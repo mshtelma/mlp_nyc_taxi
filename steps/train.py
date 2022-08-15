bc08e9654b0862e8a35f4e5e2405edaa0bf5e299
mshtelma <mshtelma>
2022-08-15 09:24:28 +0000

    changed partameters

--- steps/train.py
+++ steps/train.py
@@ -16,15 +16,15 @@ def estimator_fn():
     """
 
     return LGBMRegressor(
-        colsample_bytree=0.7201078529232644,
-        lambda_l1=0.7730308267776886,
-        lambda_l2=5.890400206116792,
-        learning_rate=0.03469799540212862,
-        max_bin=482,
-        max_depth=12,
-        min_child_samples=58,
-        n_estimators=623,
-        num_leaves=36,
-        subsample=0.7203410786015857,
-        random_state=77049990,
-    )
+              colsample_bytree=0.7201078529232644,
+              lambda_l1=0.7730308267776886,
+              lambda_l2=5.890400206116792,
+              learning_rate=0.03469799540212862,
+              max_bin=482,
+              max_depth=12,
+              min_child_samples=58,
+              n_estimators=623,
+              num_leaves=36,
+              subsample=0.7203410786015857,
+              random_state=77049990,
+            )
